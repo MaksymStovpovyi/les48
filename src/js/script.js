13086df7553e4812a904c0901ec8344444560bf9
Max <stovpovymax@gmail.com>
2022-02-14 22:13:44 +0100

    65_end

--- src/js/script.js
+++ src/js/script.js
@@ -76,4 +76,45 @@ $(document).ready(function(){
     validateForms('#consultation form');
     validateForms('#order form');
 
+    $('input[name=phone]').mask("+34 (999) 999-99-99")
+
+    // maild
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        };
+
+        $.ajax({
+            type: "POST",
+            ulr: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    // Smooth scroll and page up
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1400) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+
 });
